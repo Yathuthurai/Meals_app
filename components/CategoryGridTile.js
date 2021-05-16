3b4a3063bffb1ad18a48a5c9651a0cce142f2e3c
Yathurshan <tyathurshan@gmail.com>
2021-05-16 07:56:28 +0530

    Added Tabs

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -17,7 +17,7 @@ const CategoryGridTile = (props) => {
   }
   return (
     <View style={styles.gridItem}>
-      <TouchableCmb style={{flex: 1}} onPress={props.onSelect}>
+      <TouchableCmb style={{ flex: 1 }} onPress={props.onSelect}>
         <View
           style={{
             ...styles.container,
@@ -39,7 +39,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden",
+    elevation: 5,
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
   },
   container: {
     flex: 1,
@@ -48,7 +52,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,8 +1,10 @@
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createAppContainer } from "@react-navigation/native";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 
 import Colors from "../constants/Colors";
 
@@ -25,5 +27,10 @@ const MealsNavigator = createStackNavigator(
     },
   }
 );
+
+const MealsTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen,
+});
 
 export default createAppContainer(MealsNavigator);
